0109d27c01d1c5433071e259319ff52d7ea1360d
Colin Wakefield <59844720+colinwake@users.noreply.github.com>
2020-03-09 11:40:30 -0400

    Method Extractions
    Extracted boolean methods to make WageCalculator#calculateWage more readable
    
    Signed-off-by: Colin Wakefield <59844720+ColinWake@users.noreply.github.com>

--- src/main/java/WageCalculator.java
+++ src/main/java/WageCalculator.java
@@ -1,6 +1,28 @@
 public class WageCalculator {
     public int calculateWage(int startHour, int endHour, int bedtime) {
 
+        determineInvalidShiftTime(startHour, endHour);
+
+        if (shiftHoursBeforeBedtime(endHour)) {
+            return 12;
+        }
+
+        if (shiftHoursAfterBedtime(endHour)) {
+            return 8;
+        }
+
+        return 0;
+    }
+
+    private boolean shiftHoursAfterBedtime(int endHour) {
+        return endHour == 19;
+    }
+
+    private boolean shiftHoursBeforeBedtime(int endHour) {
+        return endHour == 18;
+    }
+
+    private void determineInvalidShiftTime(int startHour, int endHour) {
         if (startHour < 17) {
             throw new InvalidShiftTimeException("Invalid shift time");
         }
@@ -8,15 +30,5 @@ public class WageCalculator {
         if (endHour > 4 && endHour < 17) {
             throw new InvalidShiftTimeException("Invalid shift time");
         }
-
-        if (endHour == 18) {
-            return 12;
-        }
-
-        if (endHour == 19) {
-            return 8;
-        }
-
-        return 0;
     }
 }
