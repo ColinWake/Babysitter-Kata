d030b7112dc53589cba75da6d6187946c536353b
Colin Wakefield <59844720+colinwake@users.noreply.github.com>
2020-03-09 10:35:24 -0400

    First passing test
    Signed-off-by: Colin Wakefield <59844720+ColinWake@users.noreply.github.com>

---
+++ src/test/java/WageCalculatorTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+public class WageCalculatorTest {
+
+
+    @Test
+    void shouldInstantiate() {
+        WageCalculator underTest = new WageCalculator();
+    }
+
+
+}
